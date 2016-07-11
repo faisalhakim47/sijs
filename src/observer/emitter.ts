f43e09504dc9434cef67e32f822403c2a7e29fa5
Faisal Hakim <faisalhakim47@gmail.com>
2016-07-11 10:03:48 +0700

    use old EEmitter

--- src/observer/emitter.ts
+++ src/observer/emitter.ts
@@ -3,32 +3,62 @@ export interface IUnwatcher {
 }
 
 export class Emitter {
-  watchers: Map<string, Set<(...data) => void>> = new Map()
+  // watchers: Map<string, Set<(...data) => void>> = new Map()
+  watchers = {}
 
-  on(name: string, watcher: (...data) => void): IUnwatcher {
-    if (!this.watchers.has(name)) {
-      this.watchers.set(name, new Set())
-    }
-    const watchers = this.watchers.get(name)
-    watchers.add(watcher)
+  on(name: string, watcher): IUnwatcher {
+    const watchers = this.watchers[name]
+      ? this.watchers[name]
+      : (this.watchers[name] = [])
+    watcher.wi = watchers.push(watcher) - 1
     return {
-      unwatch() {
-        watchers.delete(watcher)
-      }
+      unwatch: () => this.off(name, watcher)
     }
   }
 
-  off(name: string, watcher: (...data) => void): void {
-    const watchers = this.watchers.get(name)
-    if (!watchers) return
-    watchers.delete(watcher)
+  off(name: string, watcher) {
+    const watchers = this.watchers[name]
+    if (!Array.isArray(watchers)) return
+    if (typeof watcher.wi === 'number') {
+      watchers.splice(watcher.wi, 1)
+    } else {
+      console.warn(watcher, 'does not contain watcher index (wi)')
+    }
   }
 
   emit(name: string, ...data) {
-    const watchers = this.watchers.get(name)
+    const watchers = this.watchers[name]
     if (!watchers) return
-    watchers.forEach((watcher) => {
-      watcher(...data)
-    })
+    let l = watchers.length
+    while (l--) {
+      watchers[l](...data)
+    }
   }
+
+  // on(name: string, watcher: (...data) => void): IUnwatcher {
+  //   if (!this.watchers.has(name)) {
+  //     this.watchers.set(name, new Set())
+  //   }
+  //   const watchers = this.watchers.get(name)
+  //   watchers.add(watcher)
+  //   return {
+  //     unwatch() {
+  //       watchers.delete(watcher)
+  //     }
+  //   }
+  // }
+
+  // off(name: string, watcher: (...data) => void): void {
+  //   const watchers = this.watchers.get(name)
+  //   if (!watchers) return
+  //   watchers.delete(watcher)
+  // }
+
+  // emit(name: string, ...data) {
+  //   const watchers = this.watchers.get(name)
+  //   if (!watchers) return
+  //   watchers.forEach((watcher) => {
+  //     watcher(...data)
+  //   })
+  // }
 }
