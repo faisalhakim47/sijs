6a69a4f60f333d55a41a8d9875082f28c037087c
Faisal Hakim <faisalhakim47@gmail.com>
2017-12-26 04:39:16 +0700

    remove example and experiment

--- example/data.js
+++
@@ -1,7 +0,0 @@
-export const items = [
-  { id: 1, name: 'Satu' },
-  { id: 2, name: 'Dua' },
-  { id: 3, name: 'Tiga' },
-  { id: 4, name: 'Empat' },
-  { id: 5, name: 'Lima' },
-]
--- example/pages/Dashboard.js
+++
@@ -1,9 +0,0 @@
-import { Component } from '../../src/index.js'
-
-export class Dashboard extends Component {
-  render() {
-    return this.html`
-      <p>Dashboard</p>
-    `
-  }
-}
