41b955c71434a60f25a5ff3fe42e376c889ba41b
Faisal Hakim <faisalhakim47@gmail.com>
2018-01-04 17:11:46 +0700

    save before RADICAL change

--- src/constant.ts
+++ src/constant.ts
@@ -4,6 +4,6 @@ import { Pipe } from './tools/subject.js'
 
 export const MARKER = '__sim__'
 export const INSTANCE = '__sii__'
-export type BasicDynamicPart = string | LitTag | Directive | Function
+export type BasicDynamicPart = string | number | boolean | LitTag | Directive | Function
 export type DynamicPart = BasicDynamicPart | Array<BasicDynamicPart>
 export type AsyncDynamicPart = DynamicPart | Pipe<DynamicPart>
